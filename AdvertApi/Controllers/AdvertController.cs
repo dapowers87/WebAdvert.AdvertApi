1605539497ee90b1128d2c343705bc0e1625668d
David Powers <DPowers@newgistics.com>
2019-03-06 16:12:53 -0600

    Init Commit

---
+++ AdvertApi/Controllers/AdvertController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using AdvertApi.Models;
+using AdvertApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AdvertApi.Controllers
+{
+    [ApiController]
+    [Route("api/v1/adverts")]
+    public class AdvertController : ControllerBase
+    {
+        private readonly IAdvertStorageService _advertStorageService;
+
+        public AdvertController(IAdvertStorageService advertStorageService)
+        {
+            this._advertStorageService = advertStorageService;
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType((int)HttpStatusCode.Created, Type=typeof(CreateAdvertResponse))]
+        public async Task<IActionResult> Create(AdvertModel model)
+        {
+            string recordId;
+            try
+            {
+                recordId = await _advertStorageService.Add(model);
+            }
+            catch(KeyNotFoundException)
+            {
+                return new NotFoundResult();
+            }
+            catch (Exception e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
+
+            return StatusCode((int)HttpStatusCode.Created, new CreateAdvertResponse
+            {
+                Id = recordId
+            });
+        }
+
+        [HttpPut]
+        [Route("Confirm")]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public async Task<IActionResult> Confirm(ConfirmAdvertModel model)
+        {
+            try
+            {
+                await _advertStorageService.Confirm(model);
+            }
+            catch (KeyNotFoundException)
+            {
+                return new NotFoundResult();
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+
+            return new OkResult();            
+        }
+    }
+}
